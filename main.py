310a00c60d039432cb255faf7dac693ebad74219
Rowlet <125616753+CoolGGames@users.noreply.github.com>
2023-03-26 05:32:06 +0000

    Changed a lot

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+     return render_template('index.html')
 
-@app.route('/r3s')
+@app.route('/r3s', methods=['GET', 'POST'])
 def r3s():
     return render_template('r3s.html')
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
-
-@app.route('/submit_form', methods=['GET', 'POST'])
-def submit_form():
+   if request.method == 'GET':
+     return render_template('form.html')
+   elif request.method == 'POST':    
     name = request.form['name']
     contactno = request.form['contactno']
     aspiration = request.form['aspiration']
     with open('data.txt','a') as file:
        file.write(name + ',' + contactno + ',' + aspiration + '\n')
-    return render_template('thanks.html', name=name, contactno=contactno, aspiration=aspiration)
+    return render_template('thanks.html', name=name, contactno=contactno, aspiration_2=aspiration)
 
 app.run(host='0.0.0.0', port=81)
+
